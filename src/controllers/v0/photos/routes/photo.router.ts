0ff7d34b7f6954b6012f1c543e55a053950d9eb3
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-06-20 04:07:36 +0100

    modified package.json for deployment

--- src/controllers/v0/photos/routes/photo.router.ts
+++ src/controllers/v0/photos/routes/photo.router.ts
@@ -35,28 +35,35 @@ router.get('/', async (req: Request, res: Response) => {
 
 
 router.get('/filteredimage', async (req: Request, res: Response) => {
+
     const query = req.query;
+
     if(!query.image_url){
         res.status(404).send('not found');
         return;
     }
 
     try {
+        // filter image
         const filteredFile = await filterImageFromURL(query.image_url);
+        // send file to client side
         res.sendFile(filteredFile);
-
+        
         const photoPath =  './src/util/tmp';
+        // scan folder where photos are temporarily saved
         fs.readdir(photoPath, function (error, files) {
         
             if (error) {
                  console.log('Failed to scan folder for file deletion: ' + error);
                  return;
             } 
+            // create an array of file paths from the scanned files
             files.forEach(function (file, index) {
 
-                files[index]=`${photoPath}/${file}`
+                files[index] = `${photoPath}/${file}`
                     
             });
+            // delete files by passing an array of file paths
             try{
                 deleteLocalFiles(files);
                 console.log('local files deleted');
